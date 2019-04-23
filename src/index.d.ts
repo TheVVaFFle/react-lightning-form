fb3b1186d19dd1031a33137c2b100c687c700e1d
Cory Loeffelman <cloeffelman@shelterinsurance.com>
2019-04-23 15:05:50 -0500

    Added more types

--- src/index.d.ts
+++ src/index.d.ts
@@ -11,4 +11,31 @@ declare module "create-react-form" {
   }
 
   export const Form: (props: FormProps) => React.SFC<FormProps>
+
+  export interface SectionProps {
+    title?: string;
+    children?: any;
+    data?: any;
+    columns?: number;
+  }
+
+  export const Section: (props: SectionProps) => React.SFC<SectionProps>
+
+  export enum FormComponentType {
+    Input = "input",
+    TextArea = "textarea",
+    Section = "section"
+  }
+    
+  export enum InputType {
+    Checkbox = "checkbox",
+    Dropdown = "dropdown",
+    TextArea = "textarea"
+  }  
+
+  export enum ValidationType {
+    Phone = "phone",
+    Email = "email",
+    Required = "required"
+  }
 }
