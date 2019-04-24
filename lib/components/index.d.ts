8e9dfaeede95fd663921bd635587e9d6d3bec82b
Cory Loeffelman <cloeffelman@shelterinsurance.com>
2019-04-24 10:34:20 -0500

    tsconfig fixes

--- lib/components/index.d.ts
+++ lib/components/index.d.ts
@@ -1,3 +1,13 @@
+/// <reference types="react" />
 import { Form } from "./form/form";
 import { Section } from "./section/section";
+import { ValidationType } from "../utility/validation";
+declare module 'react' {
+    interface HTMLAttributes<T> extends DOMAttributes<T> {
+        validate?: ValidationType | Function;
+        label?: string;
+        type?: string;
+        errormessage?: string;
+    }
+}
 export { Form, Section };
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,17 @@
 import {Form} from "./form/form";
 import {Section} from "./section/section";
 
+import { ValidationType } from "../utility/validation";
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    validate?: ValidationType | Function;
+    label?: string;
+    type?: string;
+    errormessage?: string;
+  }
+}
+
 export {
   Form,
   Section
