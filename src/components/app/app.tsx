c5333fa6d977ceb7f9f4a1ed5518bebf8746ea0b
Cory Loeffelman <cloeffelman@shelterinsurance.com>
2019-04-24 11:19:33 -0500

    Fixed imports

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,60 +1,48 @@
 import * as React from "react";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
-import {Form} from "../form/form";
-import {Section} from "../section/section";
+import { Form } from "../";
+import { Section } from "../";
 
-import { ValidationType } from "../../utility/validation";
+import { Test } from "../../data";
 
-import {Test} from "../../data";
+require("./app.scss");
 
-require('./app.scss');
+export interface AppProps {}
 
-export interface AppProps {
-
-}
-
-export const App: React.SFC<AppProps> = (props: AppProps) => {  
+export const App: React.SFC<AppProps> = (props: AppProps) => {
   const [loading, setLoading] = useState(true);
 
   const simulateLoading = () => {
     setLoading(true);
     setTimeout(() => setLoading(false), 1000);
-  }
+  };
 
   useEffect(() => {
     simulateLoading();
-  }, [])
+  }, []);
 
   const handleOnSubmit = (formState: any) => {
-    console.log(formState)
+    console.log(formState);
     simulateLoading();
-  }
+  };
 
   const validateComments = (value: string): boolean => {
     return value.length >= 20 && value.length <= 50;
-  }
+  };
 
-  return(
+  return (
     <div id="app">
-      <Form 
+      <Form
         id="app-form"
         title="My Form"
         loading={loading}
         onSubmit={handleOnSubmit}
       >
-        <Section 
-          title="Name" 
-          columns={4} 
-          data={Test.data.name}
-        />          
-        <Section 
-          title="Contact" 
-          columns={2} 
-          data={Test.data.contact}
-        />
-        <textarea 
-          id="comments" 
+        <Section title="Name" columns={4} data={Test.data.name} />
+        <Section title="Contact" columns={2} data={Test.data.contact} />
+        <textarea
+          id="comments"
           placeholder="Enter comments here"
           label="Comments"
           validate={validateComments}
@@ -62,5 +50,5 @@ export const App: React.SFC<AppProps> = (props: AppProps) => {
         />
       </Form>
     </div>
-  )
-}
+  );
+};
