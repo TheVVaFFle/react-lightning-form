692c017bcf2d3feaf31fe5e88701f6f6c392375c
Cory Loeffelman <cloeffelman@shelterinsurance.com>
2019-05-09 07:28:36 -0500

    New build

---
+++ lib/utility/form/index.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="react" />
+export declare const FormUtility: {
+  stateManager: any;
+  validate: any;
+  get: any;
+  input: any;
+  map: {
+    from: {
+      data: (data: any) => JSX.Element[];
+    };
+  };
+  handle: {
+    children: (fields: JSX.Element) => any;
+  };
+};
---
+++ lib/utility/form/input/mapFromChildren.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="react" />
+export declare const mapFromChildren: (fields: JSX.Element) => any;
