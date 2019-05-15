f7881b5e805f708acfa74308c3c8b990989c43ef
Cory Loeffelman <cloeffelman@shelterinsurance.com>
2019-05-15 11:22:49 -0500

    New build.

--- lib/components/table/table.d.ts
+++ lib/components/table/table.d.ts
@@ -3,6 +3,8 @@ export interface TableProps {
   id: string;
   data?: any;
   selectable?: boolean;
+  errorMessage?: string;
+  emptyMessage?: string;
   onSelect?: any;
 }
 export declare const Table: React.SFC<TableProps>;
